bbe59de4628c819305a051f41c9f8ba04ff78770
RatanAbhinav <71868905+RatanAbhinav@users.noreply.github.com>
2024-09-29 20:12:21 +0530

    Add files via upload
    Design and Test Bench files for APB Protocol

---
+++ apb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 29.09.2024 12:39:35
+// Design Name: 
+// Module Name: apb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module apb(
+
+    );
+endmodule
